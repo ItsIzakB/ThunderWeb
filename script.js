bd457fe70f6d6d2b2e457d67a5190d65066a58b5
Kazi Priom <109478175+ItsIzakB@users.noreply.github.com>
2024-01-04 19:59:44 -0600

    Update script.js
    This is day two where i added animations to website

--- script.js
+++ script.js
@@ -1,21 +1,195 @@
+function activatePopUps(){
+    var popUps = document.getElementById("pop");
+
+    var AboutHasPopOpen = popUps.classList.contains("pop");
+    if(!AboutHasPopOpen){
+        popUps.classList.add("active");
+    }
+}
+function openPopup(popupId){
+    var popup = document.getElementById(popupId);
+    popup.classList.add("PopOpen");
+}
+
+function closePopup(popupId) {
+    var popup = document.getElementById(popupId);
+    popup.classList.remove("PopOpen");
+    popup.classList.add("PopClose");
+}
+
+function checkOpenSeat(){
+    var popAbout = document.getElementById("myAbout"); 
+    var popContact = document.getElementById("myContact"); 
+
+    var AboutHasPopOpen = popAbout.classList.contains("PopOpen");
+    var ContactHasPopOpen= popContact.classList.contains("PopOpen");
+
+    if (AboutHasPopOpen){
+        popAbout.classList.remove("PopOpen");
+        popAbout.classList.add("PopClose"); 
+    }
+    else if(ContactHasPopOpen){
+        popContact.classList.remove("PopOpen");
+        popContact.classList.add("PopClose");
+    }
+} 
+
+function checkCloseSeat(){
+    var popSeat = document.getElementById("mySeat"); 
+
+    var SeatHasPopClose = popSeat.classList.contains("PopClose");
+
+    if (SeatHasPopClose){
+        popSeat.classList.remove("PopClose");
+    }
+} 
+
+
+
+
+function checkOpenContact(){
+    var popSeat = document.getElementById("mySeat"); 
+    var popAbout = document.getElementById("myAbout"); 
+
+    var SeatHasPopOpen= popSeat.classList.contains("PopOpen");
+    var AboutHasPopOpen = popAbout.classList.contains("PopOpen");
+
+    if (AboutHasPopOpen){
+        popAbout.classList.remove("PopOpen");
+        popAbout.classList.add("PopClose"); 
+    }
+    else if(SeatHasPopOpen){
+        popSeat.classList.remove("PopOpen");
+        popSeat.classList.add("PopClose");
+    }
+} 
+
+function checkCloseContact(){
+    var popContact= document.getElementById("myContact"); 
+
+    var ContactHasPopClose = popContact.classList.contains("PopClose");
+
+    if (ContactHasPopClose){
+        popContact.classList.remove("PopClose");
+    }
+} 
+
+
+function checkOpenAbout(){
+    var popContact = document.getElementById("myContact"); 
+    var popSeat = document.getElementById("mySeat"); 
+
+    var ContactHasPopOpen = popContact.classList.contains("PopOpen");
+    var SeatHasPopOpen= popSeat.classList.contains("PopOpen");
+
+    if (ContactHasPopOpen){
+        popContact.classList.remove("PopOpen");
+        popContact.classList.add("PopClose"); 
+    }
+    else if(SeatHasPopOpen){
+        popSeat.classList.remove("PopOpen");
+        popSeat.classList.add("PopClose");
+    }
+} 
+
+function checkCloseAbout(){
+    var popAbout= document.getElementById("myAbout"); 
+
+    var AboutHasPopClose = popAbout.classList.contains("PopClose");
+
+    if (AboutHasPopClose){
+        popAbout.classList.remove("PopClose");
+    }
+} 
+
+
+
 function openPopSeat() {
-    document.getElementById("myAbout").style.display = "none";
-    document.getElementById("myContact").style.display = "none";
-    document.getElementById("mySeat").style.display = "flex";
+
+    var popSeat = document.getElementById("mySeat");
+    var popAbout = document.getElementById("myAbout"); 
+    var popContact = document.getElementById("myContact"); 
+
+
+    /* for transition */
+
+    popSeat.classList.add("animatePopOpen");
+
+    popContact.classList.remove("animatePopOpen");
+    popContact.classList.add("animatePopClose");
+
+    popAbout.classList.remove("animatePopOpen");
+
+    var popUps = document.getElementById("pop");
+
+    popUps.classList.add("active");
+    
 }
 
 function openPopContact() {
-    document.getElementById("myAbout").style.display = "none";
-    document.getElementById("myContact").style.display = "flex";
-    document.getElementById("mySeat").style.display = "none";
+
+
+    var popSeat = document.getElementById("mySeat");
+    var popAbout = document.getElementById("myAbout"); 
+    var popContact = document.getElementById("myContact"); 
+
+    /* for transition */
+
+    popSeat.classList.remove("active");
+
+    popContact.classList.add("animatePopOpen");
+
+    popAbout.classList.remove("active");
+
+
+    var popUps = document.getElementById("pop");
+
+    popUps.classList.add("active");
+
 }
 
 function openPopAbout() {
-    document.getElementById("myAbout").style.display = "flex";
-    document.getElementById("myContact").style.display = "none";
-    document.getElementById("mySeat").style.display = "none";
+
+    var popSeat = document.getElementById("mySeat");
+    var popAbout = document.getElementById("myAbout"); 
+    var popContact = document.getElementById("myContact"); 
+
+    /* for transition */
+
+    popSeat.classList.remove("active");
+
+    popContact.classList.remove("active");
+
+    popAbout.classList.add("animatePopOpen");
+
+
+    var popUps = document.getElementById("pop");
+
+    popUps.classList.add("active");
+
 }
 
-document.getElementById("triggerSeat").addEventListener("click", openPopSeat);
-document.getElementById("triggerContact").addEventListener("click", openPopContact);
-document.getElementById("triggerAbout").addEventListener("click", openPopAbout);
+/* We need to check whether other pop ups are open before doing our animation
+    We also need to make sure the popClose classlist is not added, because that
+    will not allow us to add the popUp classlist after.
+    */
+document.getElementById("triggerSeat").addEventListener("click", function () {
+    activatePopUps();
+    checkOpenSeat();
+    checkCloseSeat();
+    openPopup("mySeat");
+});
+
+document.getElementById("triggerContact").addEventListener("click", function () {
+    activatePopUps();
+    checkOpenContact();
+    checkCloseContact();
+    openPopup("myContact");
+});
+
+document.getElementById("triggerAbout").addEventListener("click", function () {
+    activatePopUps();
+    checkOpenAbout();
+    checkCloseAbout();
+    openPopup("myAbout");
+});
